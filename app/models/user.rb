d477f5f4b22c6d8a2d06843347515e2ff5e171c0
ryota-narushima <r.narushima1031@gmail.com>
2020-12-24 16:55:30 +0900

    userモデルでバリデーションの設定２

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,21 @@ class User < ApplicationRecord
   validates :last_name_k, presence: true
   validates :first_name_k, presence: true
   validates :birth_day, presence: true
+
+  # 半角英数字混合のパスワード
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+
+  # ユーザー本名は、全角（漢字・ひらがな・カタカナ）での入力が必須
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
+    validates :last_name_j
+    validates :first_name_j
+  end
+
+  # ユーザー本名のフリガナは、全角（カタカナ）での入力が必須
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/ } do
+    validates :last_name_k
+    validates :first_name_k
+  end
+
 end
