e43719c04f6a30ded9045480ff822387652b4464
ryota-narushima <r.narushima1031@gmail.com>
2021-01-06 01:19:19 +0900

    商品出品ページへの遷移を制限した

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+  before_action :move_to_index, except: [:index]
 
   def index
   end
@@ -8,6 +10,14 @@ class ItemsController < ApplicationController
   end
 
   def create
+  end
+
+  private
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 
 end
