daeb3d22e27a0e250174c5e230c7c7e60828910c
ryota-narushima <r.narushima1031@gmail.com>
2021-01-06 21:05:16 +0900

    itemモデルのアソシエーションとバリデーションを設定した

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,13 @@
 class Item < ApplicationRecord
+  # アソシエーション
   has_one_attached :image
+  belongs_to :user
 
   # ActiveHashのアソシエーション
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category, :condition, :delivery_charge, :delivery_source_area, :days_to_delivery
 
-  #ジャンルの選択が「--」の時は保存できないようにする
+  #ジャンルの選択が「--」の時は保存できない
   with_options numericality: { other_than: 1 } do
     validates :category_id
     validates :condition_id
@@ -14,13 +16,20 @@ class Item < ApplicationRecord
     validates :days_to_delivery_id
   end
 
-  #空の投稿を保存できないようにする
+  #空の投稿は保存できない
+  validates :image, attached_file_presence: true
   with_options presence: true do
+    validates :name
+    validates :description
     validates :category_id
     validates :condition_id
     validates :delivery_charge_id
     validates :delivery_source_area_id
     validates :days_to_delivery_id
+    validates :price
   end
-  
+
+  # 販売価格は半角数字で¥300~¥9,999,999の範囲内しか保存できない
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  # アソシエーション
+  has_many :items
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
@@ -26,4 +29,5 @@ class User < ApplicationRecord
     validates :last_name_k
     validates :first_name_k
   end
+
 end
