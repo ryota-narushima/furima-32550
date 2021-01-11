69ca44f25b561897b3cc66834752d3f645d6b366
ryota-narushima <r.narushima1031@gmail.com>
2021-01-11 17:17:33 +0900

    指摘点の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,7 +38,8 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
-    if item.destroy
+    if item.destroy && item.user_id == current_user.id
+      
       redirect_to action: :index
     end
   end
