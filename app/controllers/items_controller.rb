06f7554910768376083eb4caf023e01644b7110a
ryota-narushima <r.narushima1031@gmail.com>
2021-01-11 17:26:59 +0900

    余計な行の削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -39,7 +39,6 @@ class ItemsController < ApplicationController
   def destroy
     item = Item.find(params[:id])
     if item.destroy && item.user_id == current_user.id
-      
       redirect_to action: :index
     end
   end
