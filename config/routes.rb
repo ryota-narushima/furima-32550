7a71efbdc5869334b721916e677a19a7bfb81162
ryota-narushima <r.narushima1031@gmail.com>
2021-01-13 15:58:58 +0900

    ビューを表示させた

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :items do
-    resources :purshases, only: :index
+    resources :purchases, only: :index
   end
 end
