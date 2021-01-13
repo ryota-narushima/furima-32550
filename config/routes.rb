048876e50c29895689d1b238579a68638b495b1c
ryota-narushima <r.narushima1031@gmail.com>
2021-01-13 11:19:24 +0900

    ルーティングの設定２

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :items do
-    resources :purshases, only: :new
+    resources :purshases, only: :index
   end
 end
